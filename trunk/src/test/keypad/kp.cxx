376e7c485b0fa36ad113fa30630310bd41697ff3
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-04-09 05:05:49 +0000

    added keypad test

---
+++ trunk/src/test/keypad/kp.cxx
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "keypad.h"
+
+int main(int argc, char **argv)
+{
+  CKeypad kp;
+
+#if 0
+  while(1)
+    printf("%x\n", kp.GetKey());
+#endif
+#if 1
+  while(1)
+    {
+      if (kp.KeyDown())
+	printf("down\n");
+      if (kp.KeyUp())
+	printf("up\n");
+      if (kp.KeyLeft())
+	printf("left\n");
+      if (kp.KeyRight())
+	printf("right\n");
+      if (kp.KeyOk())
+	printf("ok\n");
+      if (kp.KeyCancel())
+	printf("cancel\n");
+    }
+#endif
+}
+
