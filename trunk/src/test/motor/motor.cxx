d8d073d63ac538e8941caacaf24d754da644a5c0
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-04-29 18:28:52 +0000

    bemf works

--- trunk/src/test/motor/motor.cxx
+++ trunk/src/test/motor/motor.cxx
@@ -1,19 +1,60 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "9302hw.h"
+#include "qemotoruser.h"
 
 int main(int argc, char **argv)
 {
-  C9302Hardware hw;
-
+  int i = 0;
   
+  
+#if 0
+  C9302Hardware hw;
   printf("%x\n", *hw.m_fpga.Ushort(0xfd0));
+
+  *hw.m_fpga.Ushort(0x000) = 0x00ff | 0x0100; 
+  *hw.m_fpga.Ushort(0x008) = 0x0700 | 0x0030; 
+  *hw.m_fpga.Ushort(0x00a) = 0x0009;
+#endif
+#if 0
   while(1)
     {
-      *hw.m_fpga.Ushort(0x000) = 0x00f0 | 0x0100; 
+      i++;
+      // wait for measurement
+      while(*hw.m_fpga.Ushort(0x00c));
+      // wait for measurement to end
+      while(!(*hw.m_fpga.Ushort(0x00c)));
+      printf("%x\n", *hw.m_fpga.Ushort(0x200));
+      //if (i%200==0)
+      //printf("%d\n", i/200);
+    }
+#endif
+#if 0
+  while(1)
+    {
+      sleep(1);
+      printf("brake\n");
+      *hw.m_fpga.Ushort(0x000) = 0x00f0 | 0x0300; 
       sleep(1);
       *hw.m_fpga.Ushort(0x000) = 0x00f0 | 0x0200;
       sleep(1);
+      printf("coast\n");
+      *hw.m_fpga.Ushort(0x000) = 0x00f0 | 0x0000; 
+      sleep(1);
     }
-
+#endif
+#if 1
+  Axis_position pos;
+  CQEMotorUser mu(NULL, 0);
+  
+  //mu.SetPWM(0, 128);
+  mu.SetPIDVGains(0, 100, 0, 500, 0);
+  mu.SetPosition(0, 0);
+  mu.Hold(0, 1);
+  while(1)
+    {
+      pos = mu.GetPosition(0);
+      printf("%d\n", pos);
+    }
+#endif
 }
