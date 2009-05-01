a6e199a72df547d04c00baa11a41b35351ca76fb
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-05-01 03:24:32 +0000

    added led, i2c, gpio, pwm

---
+++ trunk/src/logic/vexpro/ledcont.v
@@ -0,0 +1,44 @@
+
+module LedCont(Addr, DataRd, DataWr, En, Rd, Wr, 
+				LedGreen, LedRed, Reset, Clk);
+
+   input  [1:0] Addr;
+   output [15:0] DataRd;
+	input  [15:0] DataWr;
+	input  En;
+	input  Rd;
+	input  Wr;
+	output [2:0] LedGreen;
+	output [2:0] LedRed;
+	input  Reset;
+	input  Clk;
+	
+	reg    [2:0] LedGreen;
+	reg    [2:0] LedRed;
+	reg    [15:0] DataRd;
+	
+	
+   always @(posedge Clk)
+      begin
+		if (Reset)
+			begin
+			LedGreen <= 3'b000;
+			LedRed <= 3'b000;
+			end
+		else if (Wr & En)
+         begin
+         if (Addr[1:0]==0)
+			   {LedRed, LedGreen} <= DataWr[5:0];
+			end
+      end
+
+   always @(Addr or LedGreen or LedRed)
+      begin
+	   if (Addr[1:0]==0)
+		   DataRd = {10'h000, LedRed, LedGreen};
+		else 
+		   DataRd = 16'hxxxx;
+		end
+		
+endmodule
+
--- trunk/src/logic/vexpro/vexpro.v
+++ trunk/src/logic/vexpro/vexpro.v
@@ -30,6 +30,7 @@ module MotorLogic(Control, Pwm, Measure, MotorA, MotorB, MotorC);
 		   begin
 			MotorA = 1'b0;
 			MotorB = 1'b0;
+			MotorC = 1'b0;
 			end
       else if (Control==2'b01) // forward
 		   begin
@@ -49,6 +50,7 @@ endmodule
 
 module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk, 
    Async, Asdo, Arstn, Asdi, AbitClk, Audio,
+	I2CDataOut, I2CDataIn, I2CClkOut, I2CClkIn, 
 	P, PIn, PClkIn);
 
 	input  [11:0] Addr;
@@ -68,11 +70,16 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
 	input  AbitClk;
 	output Audio;
 	
+	output I2CDataOut;
+	input  I2CDataIn;
+	output I2CClkOut; 
+	input  I2CClkIn; 
+	
 	inout  [52:0] P;
 	input  [4:0] PIn;
 	input  PClkIn;
 
-	wire   [15:0] DataRd;
+	reg    [15:0] DataRd;
 
    wire   Rd;
 	wire   Wr;
@@ -81,17 +88,17 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
 
 	wire BemfIntStatus;
 	wire BemfIntReset;
-	wire [7:0] GpioIntStatus;
-	wire [7:0] GpioIntReset;
-	wire [6:0] IntDummy;
+	wire GpioIntStatus;
+	wire GpioIntReset;
+	wire [13:0] IntDummy;
 	reg  TestIntStatus;
 	wire TestIntReset;
 	Primary InstPrimary(.Addr(Addr), .Data(Data), .RdN(RdN), .WrN(WrN), .Dq(Dq), 
 	   .CsN(CsN), .Wait(Wait), .Int(Int), .Clk(Clk), 
       .Async(Async), .Asdo(Asdo), .Arstn(Arstn), .Asdi(Asdi), .AbitClk(AbitClk), .AudioOut(Audio),
 		.Rd(Rd), .Wr(Wr), .Cs(Cs), .DataRd(DataRd), .Identifier(16'hc002), .Reset(Reset),
-		.IntStatus({GpioIntStatus, 7'h00, BemfIntStatus}), 
-		.IntReset({GpioIntReset, IntDummy, BemfIntReset}));
+		.IntStatus({14'h0000, GpioIntStatus, BemfIntStatus}), 
+		.IntReset({IntDummy, GpioIntReset, BemfIntReset}));
 
 	wire BemfEn;
 	wire [15:0]BemfDataRd;
@@ -111,6 +118,7 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
 		.IntStatus(BemfIntStatus), .IntReset(BemfIntReset), 
 		.Reset(Reset), .Clk(Clk)); 
 		
+`ifdef foo
 	assign P[0] = Active[0];
 	assign P[1] = Active[1];
 	assign P[2] = Active[2];
@@ -119,6 +127,7 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
 	assign P[5] = Measure[1];
 	assign P[6] = Measure[2];
 	assign P[7] = Measure[3];
+`endif
 
 	// ME0 P13
 	// MP0 P18
@@ -138,15 +147,24 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
 	MotorLogic Inst3MotorLogic(.Control(PwmCont[7:6]), .Pwm(PwmOut[3]), .Measure(~Active[3]),
 		.MotorA(P[37]), .MotorB(P[38]), .MotorC(P[39]));
 
-`ifdef foo
+   // I2C
+	wire I2cEn;
+	wire [15:0] I2cDataRd;
+   assign I2cEn = Cs & Addr[11:6]==6'b010010;
+
+	I2cCont InstI2cCont(.Addr(Addr[3:1]), .DataRd(I2cDataRd), .DataWr(Data), .En(I2cEn), .Rd(Rd), .Wr(Wr), 
+	   .SdaOut(I2CDataOut), .SdaIn(I2CDataIn), .SclOut(I2CClkOut), .SclIn(I2CClkIn), 
+//		.IntStatus(I2cIntStatus), .IntReset(I2cIntReset), 
+	   .Reset(Reset), .Clk(Clk));
+
 	// RC Servo
 	wire RcsEn;
 	wire [15:0] RcsDataRd;
    assign RcsEn = Cs & Addr[11:6]==6'b010000;
 
-	RCServo #(16) InstRCServo(.Addr(Addr[5:1]), .DataRd(RcsDataRd), .DataWr(Data), .En(RcsEn), .Rd(Rd), .Wr(Wr), 
-	   .P({P[51], P[52], P[50], P[48], P[46], P[44], P[43], P[41], 
-		    P[39], P[37], P[35], P[33], P[32], P[30], P[28], P[26]}), 
+	RCServo #(12) InstRCServo(.Addr(Addr[5:1]), .DataRd(RcsDataRd), .DataWr(Data), .En(RcsEn), .Rd(Rd), .Wr(Wr), 
+	   .P({P[27], P[26], P[25], P[24], P[23], P[22], P[21], P[20], 
+		    P[19], P[18], P[17], P[16]}), 
 		.Clk(Clk));
 
 	// GpioInt
@@ -155,34 +173,33 @@ module Vexpro(Addr, Data, RdN, WrN, Dq, CsN, Wait, Int, Clk,
    assign GpioEn = Cs & Addr[11:6]==6'b010001;
 
 	GpioInt InstGpioInt(.Addr(Addr[3:1]), .DataRd(GpioDataRd), .DataWr(Data), .En(GpioEn), .Rd(Rd), .Wr(Wr), 
-	   .PortIn({PIn[0], P[4], P[2], P[0], P[7], P[5], P[3], P[1]}), 
-		.PortOut({P[11], P[10], P[9], P[6], P[16], P[14], P[12], P[8]}), 
+		.Port({P[15], P[14], P[13], P[12], P[11], P[10], P[9], P[8],
+		       P[7],  P[6],  P[5],  P[4],  P[3],  P[2],  P[1], P[0]}), 
 		.IntStatus(GpioIntStatus), .IntReset(GpioIntReset), .Reset(Reset), .Clk(Clk));
 
-   // I2C
-	wire I2cEn;
-	wire [15:0] I2cDataRd;
-   assign I2cEn = Cs & Addr[11:6]==6'b010010;
 
-	I2cCont InstI2cCont(.Addr(Addr[3:1]), .DataRd(I2cDataRd), .DataWr(Data), .En(I2cEn), .Rd(Rd), .Wr(Wr), 
-	   .SdaOut(P[49]), .SdaIn(PIn[4]), .SclOut(P[47]), .SclIn(), 
-//		.IntStatus(I2cIntStatus), .IntReset(I2cIntReset), 
-	   .Reset(Reset), .Clk(Clk));
+   // LedCont
+	wire LedEn;
+	wire [15:0] LedDataRd;
+   assign LedEn = Cs & Addr[11:6]==6'b010011;
+
+	LedCont InstLedCont(.Addr(Addr[2:1]), .DataRd(LedDataRd), .DataWr(Data), .En(LedEn), .Rd(Rd), .Wr(Wr), 
+			.LedGreen({P[47], P[45], P[43]}), .LedRed({P[48], P[46], P[44]}), .Reset(Reset), .Clk(Clk));
 
 	always @(BemfDataRd or BemfEn or
 	         RcsDataRd or RcsEn or
 				GpioDataRd or GpioEn or
-				I2cDataRd or I2cEn)
+				I2cDataRd or I2cEn or
+				LedDataRd or LedEn)
 	   begin
-		case ({BemfEn, RcsEn, GpioEn, I2cEn})
-		   4'b1000: DataRd = BemfDataRd;
-			4'b0100: DataRd = RcsDataRd;
-			4'b0010: DataRd = GpioDataRd;
-			4'b0001: DataRd = I2cDataRd;
+		case ({BemfEn, RcsEn, GpioEn, I2cEn, LedEn})
+		   5'b10000: DataRd = BemfDataRd;
+			5'b01000: DataRd = RcsDataRd;
+			5'b00100: DataRd = GpioDataRd;
+			5'b00010: DataRd = I2cDataRd;
+			5'b00011: DataRd = LedDataRd;
 		   default: DataRd = 16'hxxxx;
 		endcase
 		end
-`endif
-	assign DataRd = BemfEn ? BemfDataRd : 16'hxxxx;
 		 
 endmodule
