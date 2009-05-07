ccfa3d35f2f6dd5778b43914c390a69c74fb4355
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-05-07 16:53:39 +0000

    refactored by moving #defines to static const class members.

--- trunk/src/libqwerk/keypad.cxx
+++ trunk/src/libqwerk/keypad.cxx
@@ -1,10 +1,17 @@
 #include <unistd.h>
 #include "keypad.h"
 
+const unsigned int CKeypad::KEY_OK = 0x0002;
+const unsigned int CKeypad::KEY_CANCEL = 0x0004;
+const unsigned int CKeypad::KEY_RIGHT = 0x0008;
+const unsigned int CKeypad::KEY_LEFT = 0x0010;
+const unsigned int CKeypad::KEY_UP = 0x0020;
+const unsigned int CKeypad::KEY_DOWN = 0x0040;
+
 CKeypad::CKeypad()
 {
   m_p9302hw = C9302Hardware::GetObject();
-  
+
   // set as input
   *m_p9302hw->PortFDataDR() &= ~0x0e;
   *m_p9302hw->PortADataDR() &= ~0x70;
@@ -24,7 +31,7 @@ unsigned int CKeypad::GetKey(bool wait)
       key = *m_p9302hw->PortFData() & 0x0e;
       key |=  *m_p9302hw->PortAData() & 0x70;
       key ^= 0x7c;
-     
+
       if (!wait || key)
 	break;
     }
@@ -39,7 +46,7 @@ void CKeypad::KeyRelease()
 {
   // wait for debouncing to dissipate
   usleep(10000);
-  
+
   while(GetKey(false))
     usleep(10000);
 }
--- trunk/src/libqwerk/keypad.h
+++ trunk/src/libqwerk/keypad.h
@@ -3,53 +3,54 @@
 
 #include "9302hw.h"
 
-#define KP_OK             0x0002
-#define KP_CANCEL         0x0004
-#define KP_RIGHT          0x0008
-#define KP_LEFT           0x0010
-#define KP_UP             0x0020
-#define KP_DOWN           0x0040
-
 class CKeypad
-{
-public:
-  CKeypad();
-  ~CKeypad();
+   {
+   public:
 
-  // returns 0 if no key is being pressed
-  // or waits until key is pressed and returns key value
-  unsigned int GetKey(bool wait=true);
+      static const unsigned int KEY_OK;
+      static const unsigned int KEY_CANCEL;
+      static const unsigned int KEY_RIGHT;
+      static const unsigned int KEY_LEFT;
+      static const unsigned int KEY_UP;
+      static const unsigned int KEY_DOWN;
 
-  // wait for key to be released-- can be used with methods below
-  void KeyRelease();
+      CKeypad();
+      ~CKeypad();
 
-  inline bool KeyOk()
-  {
-    return GetKey(false)&KP_OK;
-  }
-  inline bool KeyCancel()
-  {
-    return GetKey(false)&KP_CANCEL;
-  }
-  inline bool KeyUp()
-  {
-    return GetKey(false)&KP_UP;
-  }
-  inline bool KeyDown()
-  {
-    return GetKey(false)&KP_DOWN;
-  }
-  inline bool KeyLeft()
-  {
-    return GetKey(false)&KP_LEFT;
-  }
-  inline bool KeyRight()
-  {
-    return GetKey(false)&KP_RIGHT;
-  }
+      // returns 0 if no key is being pressed
+      // or waits until key is pressed and returns key value
+      unsigned int GetKey(bool wait = true);
 
-private:
-  C9302Hardware *m_p9302hw;
-};
+      // wait for key to be released-- can be used with methods below
+      void KeyRelease();
+
+      inline bool KeyOk()
+         {
+         return GetKey(false) & KEY_OK;
+         }
+      inline bool KeyCancel()
+         {
+         return GetKey(false) & KEY_CANCEL;
+         }
+      inline bool KeyUp()
+         {
+         return GetKey(false) & KEY_UP;
+         }
+      inline bool KeyDown()
+         {
+         return GetKey(false) & KEY_DOWN;
+         }
+      inline bool KeyLeft()
+         {
+         return GetKey(false) & KEY_LEFT;
+         }
+      inline bool KeyRight()
+         {
+         return GetKey(false) & KEY_RIGHT;
+         }
+
+   private:
+      C9302Hardware *m_p9302hw;
+   };
 
 #endif
