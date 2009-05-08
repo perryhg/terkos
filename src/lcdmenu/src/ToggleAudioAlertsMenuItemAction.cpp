3e0cfa613302c1475648989d6aca2e0bce190247
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-05-08 01:02:21 +0000

    Added ToggleAudioAlertsMenuItemAction (doesn't yet read/write from/to config file)

---
+++ src/lcdmenu/src/ToggleAudioAlertsMenuItemAction.cpp
@@ -0,0 +1,51 @@
+/*
+ * Chris Bartley (bartley@cmu.edu)
+ */
+
+#include "ToggleAudioAlertsMenuItemAction.h"
+
+const string ToggleAudioAlertsMenuItemAction::CLASS_NAME = "ToggleAudioAlertsMenuItemAction";
+
+bool ToggleAudioAlertsMenuItemAction::shouldOption1BeSelectedUponActivation() const
+   {
+   // TODO: fetch this from wherever we do persistence
+   return isAlertsEnabled;
+   }
+
+void ToggleAudioAlertsMenuItemAction::executeOption1Action()
+   {
+   setAlertsEnabled(true);
+   }
+
+void ToggleAudioAlertsMenuItemAction::executeOption2Action()
+   {
+   setAlertsEnabled(false);
+   }
+
+void ToggleAudioAlertsMenuItemAction::setAlertsEnabled(const bool isAlertsEnabled)
+   {
+   // TODO: write this this to wherever we do persistence
+   this->isAlertsEnabled = isAlertsEnabled;
+   }
+
+// required definitions for dynamic loading
+extern "C"
+   {
+   void deleteObject(void* obj)
+      {
+      delete reinterpret_cast<DynamicObject*> (obj);
+      }
+
+   void* loadObject(const char* name, int argc, void** argv)
+      {
+      int numCharsToCompare = std::min(strlen(name), strlen(ToggleAudioAlertsMenuItemAction::CLASS_NAME.c_str()));
+      if (strncmp(name, ToggleAudioAlertsMenuItemAction::CLASS_NAME.c_str(), numCharsToCompare) == 0)
+         {
+         map<string, string> properties;
+         return new ToggleAudioAlertsMenuItemAction(deleteObject, (MenuItem*) argv[0], (MenuStatusManager*) argv[1], (CharacterDisplay*) argv[2], *((map<
+                  string, string>*) argv[3]));
+         }
+
+      return NULL;
+      }
+   }
