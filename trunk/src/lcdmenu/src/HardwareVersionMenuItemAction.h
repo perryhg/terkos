b69884bf05ee85710b3e053654eaad68ef7a7ec3
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-05-11 16:22:35 +0000

    HardwareVersionMenuItemAction and HardwareVersionMenuItemAction can now use the "label" property defined in the menu.xml

--- trunk/src/lcdmenu/src/HardwareVersionMenuItemAction.h
+++ trunk/src/lcdmenu/src/HardwareVersionMenuItemAction.h
@@ -1,6 +1,12 @@
-/*
+/**
+ * The HardwareVersionMenuItemAction class is a CharacterDisplayMenuItemAction for viewing the hardware version.
+ *
+ * Users and subclasses can override the default label ("Hardware version") by constructing the instance with a map
+ * containing the key "label".  The value for that key will be used instead of the default.
+ *
  * Chris Bartley (bartley@cmu.edu)
  */
+
 
 #ifndef HARDWAREVERSIONMENUITEMACTION_H_
 #define HARDWAREVERSIONMENUITEMACTION_H_
@@ -9,6 +15,7 @@
 #include <string.h>
 #include "CharacterDisplay.h"
 #include "CharacterDisplayMenuItemAction.h"
+//#include "qwerkhw.h"
 
 using namespace std;
 
@@ -17,6 +24,8 @@ class HardwareVersionMenuItemAction : public CharacterDisplayMenuItemAction
    public:
 
       static const string CLASS_NAME;
+      static const string PROPERTY_LABEL;
+      static const string DEFAULT_LABEL;
 
       HardwareVersionMenuItemAction(void(*delObj)(void*), MenuItem* menuItem, MenuStatusManager* menuStatusManager,
                                     CharacterDisplay* characterDisplay, map<string, string>& properties) :
@@ -31,6 +40,10 @@ class HardwareVersionMenuItemAction : public CharacterDisplayMenuItemAction
          }
 
       virtual void activate();
+
+   private:
+
+      //CQwerkHardware hardware;
    };
 
 #endif /* HARDWAREVERSIONMENUITEMACTION_H_ */
