374dbbfdafcf5618c1d72495acc6c4284b371ed2
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-05-13 01:56:39 +0000

    added various tests

---
+++ trunk/src/test/analog/analog.cxx
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "9302hw.h"
+#include "qwerkhw.h"
+
+int main(int argc, char **argv)
+{
+  C9302Hardware *phw=C9302Hardware::GetObject();
+
+  // turn on I/O 5V regulator
+  *phw->PortHDataDR() |= 0x0020;  
+  *phw->PortHData() |= 0x0020;
+
+#if 0
+
+  //set channel
+  *phw->PortHDataDR() |= 0x001c;
+  
+  *phw->PortHData() &= ~0x001c;
+
+  while(1)
+    printf("%x\n", hw.GetAD(1));
+#else
+  CQwerkHardware hw;
+
+  while(1)
+#if 1
+    printf("%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n",
+	   hw.GetADRaw(0), hw.GetADRaw(1), hw.GetADRaw(2), hw.GetADRaw(3),
+	   hw.GetADRaw(4), hw.GetADRaw(5), hw.GetADRaw(6), hw.GetADRaw(7),
+	   hw.GetADRaw(8), hw.GetADRaw(9), hw.GetADRaw(10), hw.GetADRaw(11),
+	   hw.GetADRaw(12), hw.GetADRaw(13), hw.GetADRaw(14), hw.GetADRaw(15));
+#endif
+    printf("%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\n",
+	   hw.GetADRaw(16), hw.GetADRaw(17), hw.GetADRaw(18), hw.GetADRaw(19),
+	   hw.GetADRaw(20), hw.GetADRaw(21), hw.GetADRaw(22), hw.GetADRaw(23));
+
+#endif
+}
+
