c4275e62637ec9c3765b8dd53f50f6d974cb3b7f
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-06-03 21:59:35 +0000

    Added initial version of ViewWirelessNetworksMenuItemAction.  This merely enables the user to view all wireless networks.  Next will be to allow them to view details and optionally set one as the "favorite".  This version doesn't do any sorting (e.g. by signal strength), nor does it filter out encrypted networks.

---
+++ src/lcdmenu/src/ViewWirelessNetworksMenuItemAction.h
@@ -0,0 +1,67 @@
+/*
+ * Chris Bartley (bartley@cmu.edu)
+ */
+
+#ifndef VIEWWIRELESSNETWORKSMENUITEMACTION_H_
+#define VIEWWIRELESSNETWORKSMENUITEMACTION_H_
+
+#include <algorithm>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <pstream.h>
+#include <json/json.h>
+#include <CharacterDisplayMenuItemAction.h>
+#include <StringUtilities.h>
+
+using namespace std;
+using namespace redi;
+
+class ViewWirelessNetworksMenuItemAction : public CharacterDisplayMenuItemAction
+   {
+   public:
+
+      static const string CLASS_NAME;
+
+      ViewWirelessNetworksMenuItemAction(void(*delObj)(void*), MenuItem* menuItem, MenuStatusManager* menuStatusManager,
+                                            CharacterDisplay* characterDisplay, map<string, string>& properties) :
+         CharacterDisplayMenuItemAction(delObj, menuItem, menuStatusManager, characterDisplay, properties)
+         {
+         currentAccessPointIndex = 0;
+         isDetailViewMode = false;
+         }
+
+      virtual ~ViewWirelessNetworksMenuItemAction()
+         {
+         currentMenuItemAction = NULL;
+         delete listModeMenuItemAction;
+         delete detailModeMenuItemAction;
+         }
+
+      void activate();
+      void start();
+      void stop();
+      void upEvent();
+      void downEvent();
+      void rightEvent();
+      void leftEvent();
+
+   private:
+
+      Json::Value accessPoints;
+
+      unsigned int currentAccessPointIndex;
+
+      bool isDetailViewMode;
+
+      MenuItemAction* currentMenuItemAction;
+      MenuItemAction* listModeMenuItemAction;
+      MenuItemAction* detailModeMenuItemAction;
+
+      void displayAccessPointDetails();
+
+      const unsigned int getNumberOfAccessPoints() const;
+
+   };
+
+#endif /* VIEWWIRELESSNETWORKSMENUITEMACTION_H_ */
