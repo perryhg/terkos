501f14819da51f2f917f905a2543333e0cd4437c
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-06-09 02:48:56 +0000

    Added getWirelessNetworkingStatusAsJSON.pl script which returns whether the adapter is plugged in, connected, and details about the connection (SSID, ip address, MAC address of adapter and access point).  Also refactored the getWirelessNetworksAsJSON.pl script, extracting stuff into the httpUtils.pl and jsonUtils.pl scripts.  Also updated the wirelessNetworking.html to show the wireless status upon load.

--- src/scripts/getWirelessNetworksAsJSON.pl
+++ src/scripts/getWirelessNetworksAsJSON.pl
@@ -23,7 +23,7 @@ while ($lineNumber < $#lines)
    next unless length($line);    # anything left?
 
    # test the line with regex to see if it's something we care about
-   if ($line =~ /^Cell\s+\d+\s+-\s+Address:\s([A-F0-9:]+)$/)
+   if ($line =~ /^Cell\s+\d+\s+-\s+Address:\s(([A-F0-9]{2}:){5}[A-F0-9]{2})$/)
       {
       # We found a new Cell line, so increment the index in the accessPoints array
       $i++;
@@ -73,119 +73,48 @@ for $j (0 .. $#accessPoints)
    $wirelessNetworks{$ssid}{access-points}{$mac}{signalLevel} = $accessPoints[$j]{signalLevel};
    }
 
-# See whether this script is being called as a CGI.  If so, look for the JSON "callback" query
-# string parameter and then output appropriate headers before printing out the JSON.
-my $callbackFunctionName = '';
-if (exists($ENV{SERVER_SOFTWARE}) && exists($ENV{REQUEST_METHOD}))
-   {
-   # parse the query string (if any) and look for the "callback" parameter for the JSON
-   if (exists($ENV{QUERY_STRING}) && ((uc($ENV{REQUEST_METHOD}) eq "GET") || (uc($ENV{REQUEST_METHOD}) eq "POST")))
-      {
-      %queryParams = parseQueryParameters();
-
-      if (exists($queryParams{callback}))
-         {
-         $callbackFunctionName = $queryParams{callback};
-         }
-      }
-
-   # print headers
-   print "Content-Type: application/json\n";
-   print "Expires: Wed, 25 Apr 2007 14:00:00 GMT\n";
-   print "Cache-Control: no-cache\n\n";
-   }
-
-# iterate over the wireless networks and print them out as JSON
-if ($callbackFunctionName)
-   {
-   print $callbackFunctionName . '(';
-   }
-print '{"wireless-networks" : [' . "\n";
+# iterate over the wireless networks and create the JSON representation
+my $json = '';
+$json .= '{"wireless-networks" : [' . "\n";
 my @ssids = sort keys %wirelessNetworks;
 for $networkCount (0 .. $#ssids)
    {
    my $ssid = $ssids[$networkCount];
-   print "   {\n";
-   print '      "ssid" : "'        . $ssid        . '"' . ",\n";
-   print '      "is-encrypted" : '  . $wirelessNetworks{$ssid}{isEncrypted} . ",\n";
-   print '      "access-points" : ['  .  "\n";
+   $json .= "   {\n";
+   $json .= '      "ssid" : "'        . $ssid        . '"' . ",\n";
+   $json .= '      "is-encrypted" : '  . $wirelessNetworks{$ssid}{isEncrypted} . ",\n";
+   $json .= '      "access-points" : ['  .  "\n";
    my @macs = sort keys %{$wirelessNetworks{$ssid}{access-points}};
    for $macCount (0 .. $#macs)
       {
-      print "         {\n";
+      $json .= "         {\n";
       $mac = $macs[$macCount];
-      print '            "mac" : "'         . $mac                                                       . '"' . ",\n";
-      print '            "channel" : '     . $wirelessNetworks{$ssid}{access-points}{$mac}{channel}     . '' . ",\n";
-      print '            "frequency" : "'   . $wirelessNetworks{$ssid}{access-points}{$mac}{frequency}   . '"' . ",\n";
-      print '            "quality" : '     . $wirelessNetworks{$ssid}{access-points}{$mac}{quality}     . '' . ",\n";
-      print '            "signalLevel" : ' . $wirelessNetworks{$ssid}{access-points}{$mac}{signalLevel} . '' . "\n";
-      print "         }";
+      $json .= '            "mac-address" : "'. $mac                                                       . '"' . ",\n";
+      $json .= '            "channel" : '     . $wirelessNetworks{$ssid}{access-points}{$mac}{channel}     . '' . ",\n";
+      $json .= '            "frequency" : "'  . $wirelessNetworks{$ssid}{access-points}{$mac}{frequency}   . '"' . ",\n";
+      $json .= '            "quality" : '     . $wirelessNetworks{$ssid}{access-points}{$mac}{quality}     . '' . ",\n";
+      $json .= '            "signalLevel" : ' . $wirelessNetworks{$ssid}{access-points}{$mac}{signalLevel} . '' . "\n";
+      $json .= "         }";
       if ($macCount < $#macs)
          {
-         print ",";
+         $json .= ",";
          }
-      print "\n";
+      $json .= "\n";
       }
-   print "      ]\n";
-   print "   }";
+   $json .= "      ]\n";
+   $json .= "   }";
    if ($networkCount < $#ssids)
       {
-      print ",";
+      $json .= ",";
       }
-   print "\n";
+   $json .= "\n";
    }
-print "]}";
+$json .= "]}";
 if ($callbackFunctionName)
    {
-   print ')';
+   $json .= ')';
    }
-print "\n";
+$json .= "\n";
 
-#===================================================================================================
-# Code taken from Matt's Scripts: http://www.scriptarchive.com/
-sub parseQueryParameters()
-   {
-   my %queryParameters = ();
-
-   # Determine the form's REQUEST_METHOD (GET or POST) and split the form
-   # fields up into their name-value pairs.  If the REQUEST_METHOD was
-   # not GET or POST, simply return and empty hash.
-   my @pairs = ();
-   if ($ENV{'REQUEST_METHOD'} eq 'GET')
-      {
-      # Split the name-value pairs
-      @pairs = split(/&/, $ENV{'QUERY_STRING'});
-      }
-   elsif ($ENV{'REQUEST_METHOD'} eq 'POST')
-      {
-      # Get the input
-      read(STDIN, $buffer, $ENV{'CONTENT_LENGTH'});
-
-      # Split the name-value pairs
-      @pairs = split(/&/, $buffer);
-      }
-   else
-      {
-      return %queryParameters;
-      }
-
-   foreach $pair (@pairs)
-      {
-      # Split the pair up into individual variables.
-      local($key, $value) = split(/=/, $pair);
-
-      # Decode the form encoding on the name and value variables.
-      $key =~ tr/+/ /;
-      $key =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
-      $key =~ tr/\0//d;
-
-      $value =~ tr/+/ /;
-      $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
-      $value =~ tr/\0//d;
-
-      $queryParameters{$key}= $value;
-      }
-
-   return %queryParameters;
-   }
-#===================================================================================================
+# write out the JSON
+outputJson($json);
