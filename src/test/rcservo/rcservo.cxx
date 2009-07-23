d96cc2c76f3227c7df89b51a4d3bb0ac5c005b32
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-07-23 15:19:48 +0000

    rewrote

--- src/test/rcservo/rcservo.cxx
+++ src/test/rcservo/rcservo.cxx
@@ -1,32 +1,29 @@
 #include <stdio.h>
-#include "rcservo.h"
+#include "qeservo.h"
 
 int main()
 {
   int i;
   volatile unsigned int d;
-  CRCServo servo;
-
- for (i=0; i<16; i++)
-    servo.SetBounds(i, 32, 228);
+  CQEServo servo;
 
   while(1)
     {
-      for (i=0; i<16; i++)
-	servo.SetPosition(i, 0x00);
-      printf("Pos: 0x%x\n", servo.GetPosition(0));
+      for (i=0; i<1; i++)
+	servo.SetPosition(i, 0);
+      printf("Pos: %d\n", servo.GetPosition(0));
       for (d=0; d<30000000; d++);
 
       // move middle
-      for (i=0; i<16; i++)
-	servo.SetPosition(i, 0x80);
-      printf("Pos: 0x%x\n", servo.GetPosition(0));
+      for (i=0; i<1; i++)
+	servo.SetPosition(i, 125);
+      printf("Pos: %d\n", servo.GetPosition(0));
       for (d=0; d<30000000; d++);
 
       // move maximum clockwise
-      for (i=0; i<16; i++)
-	servo.SetPosition(i, 0xff);
-      printf("Pos: 0x%x\n", servo.GetPosition(0));
+      for (i=0; i<1; i++)
+	servo.SetPosition(i, 250);
+      printf("Pos: %d\n", servo.GetPosition(0));
       for (d=0; d<30000000; d++);
     }
 }
