e80bfbbd25a995bd6e09a5c79f1260a447f64b0a
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-07-30 01:37:09 +0000

    rewrote led class for vex

--- trunk/src/test/analog/analog.cxx
+++ trunk/src/test/analog/analog.cxx
@@ -3,40 +3,39 @@
 #include "9302hw.h"
 #include "qwerkhw.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
-  C9302Hardware *phw=C9302Hardware::GetObject();
-
-  // turn on I/O 5V regulator
-  *phw->PortHDataDR() |= 0x0020;  
-  *phw->PortHData() |= 0x0020;
-
-  // *phw->PortCData() = 0; // turn off unit
+  unsigned short val;
+  C9302Hardware *p9302hw=C9302Hardware::GetObject();
+  CQwerkHardware *phw=CQwerkHardware::GetObject();
 
 #if 0
-
-  //set channel
-  *phw->PortHDataDR() |= 0x001c;
-  
-  *phw->PortHData() &= ~0x001c;
-
   while(1)
-    printf("%x\n", hw.GetAD(1));
-#else
-  CQwerkHardware hw;
-
-  while(1)
+    {
+      val = p9302hw->GetAD(4);
+      printf("%d\n", val);
+    }
+#endif
 #if 1
+  while(1)
+    {
+      val = phw->GetADVoltage(24);
+      printf("%d\n", val);
+    }
+#endif
+
+#if 0
+  while(1)
+    {
     printf("%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\t\n",
 	   hw.GetADRaw(0), hw.GetADRaw(1), hw.GetADRaw(2), hw.GetADRaw(3),
 	   hw.GetADRaw(4), hw.GetADRaw(5), hw.GetADRaw(6), hw.GetADRaw(7),
 	   hw.GetADRaw(8), hw.GetADRaw(9), hw.GetADRaw(10), hw.GetADRaw(11),
 	   hw.GetADRaw(12), hw.GetADRaw(13), hw.GetADRaw(14), hw.GetADRaw(15));
-#endif
     printf("%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\n",
 	   hw.GetADRaw(16), hw.GetADRaw(17), hw.GetADRaw(18), hw.GetADRaw(19),
 	   hw.GetADRaw(20), hw.GetADRaw(21), hw.GetADRaw(22), hw.GetADRaw(23));
-
+    }
 #endif
 }
 
