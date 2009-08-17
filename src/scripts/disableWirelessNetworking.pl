f12e4e50ace78658adbaadaa5c39f818cbebaeeb
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-08-17 14:19:43 +0000

    fixed typo in comment

--- src/scripts/disableWirelessNetworking.pl
+++ src/scripts/disableWirelessNetworking.pl
@@ -4,7 +4,7 @@ require "/opt/scripts/wirelessNetworkingUtils.pl";
 
 my $interfaceName = "wlan0";
 
-# call ifdown shut down the wireless interface
+# call ifdown to shut down the wireless interface
 open(IFDOWN_STATUS, "/sbin/ifdown $interfaceName |") or die "Failed to call ifdown $!\n";
 close(IFDOWN_STATUS);
 
