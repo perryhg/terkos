eb44625ed02bf27b4b2a6e0c066aa7dd8eedd8c8
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-08-17 02:07:05 +0000

    Refactored version info stuff (hardware, firmware, OS) into a single VersionInfoMenuItemAction.

---
+++ src/lcdmenu/src/VersionInfoMenuItemAction.h
@@ -0,0 +1,76 @@
+/*
+ * Chris Bartley (bartley@cmu.edu)
+ */
+
+#ifndef VERSIONINFOMENUITEMACTION_H_
+#define VERSIONINFOMENUITEMACTION_H_
+
+#include <algorithm>
+#include <string.h>
+#include <unistd.h>
+#include <json/json.h>
+#include "CharacterDisplayMenuItemAction.h"
+#include "VersionInfoManager.h"
+#include "StringUtilities.h"
+
+class VersionInfoMenuItemAction : public CharacterDisplayMenuItemAction
+   {
+   public:
+
+      static const string CLASS_NAME;
+
+      VersionInfoMenuItemAction(void(*delObj)(void*), MenuItem* menuItem, MenuStatusManager* menuStatusManager, CharacterDisplay* characterDisplay,
+                                map<string, string>& properties) :
+         CharacterDisplayMenuItemAction(delObj, menuItem, menuStatusManager, characterDisplay, properties)
+         {
+         VersionInfoManager versionInfoManager;
+
+         unsigned int numColumnsWhenInScrollingMode = characterDisplay->getColumnsWhenInScrollingMode();
+
+         string screen0Label = getProperty(OPERATING_SYSTEM_VERSION_PROPERTY, OPERATING_SYSTEM_VERSION_DEFAULT_LABEL);
+         screens.push_back(StringUtilities::rightPadOrTrimRight(screen0Label + ":", numColumnsWhenInScrollingMode)
+                  + versionInfoManager.getOperatingSystemName() + " " + versionInfoManager.getOperatingSystemRelease());
+
+         string screen1Label = getProperty(FIRMWARE_VERSION_PROPERTY, FIRMWARE_VERSION_DEFAULT_LABEL);
+         screens.push_back(StringUtilities::rightPadOrTrimRight(screen1Label + ":", numColumnsWhenInScrollingMode)
+                  + versionInfoManager.getFirmwareVersion());
+
+         string screen2Label = getProperty(HARDWARE_VERSION_PROPERTY, HARDWARE_VERSION_DEFAULT_LABEL);
+         screens.push_back(StringUtilities::rightPadOrTrimRight(screen2Label + ":", numColumnsWhenInScrollingMode)
+                  + versionInfoManager.getHardwareVersion());
+         }
+
+      virtual ~VersionInfoMenuItemAction()
+         {
+         // nothing to do
+         }
+
+      void activate();
+      void start();
+      void stop();
+      void upEvent();
+      void downEvent();
+      void rightEvent();
+      void leftEvent();
+
+   private:
+
+      static const string UNKNOWN_VALUE;
+
+      static const string OPERATING_SYSTEM_VERSION_PROPERTY;
+      static const string FIRMWARE_VERSION_PROPERTY;
+      static const string HARDWARE_VERSION_PROPERTY;
+
+      static const string OPERATING_SYSTEM_VERSION_DEFAULT_LABEL;
+      static const string FIRMWARE_VERSION_DEFAULT_LABEL;
+      static const string HARDWARE_VERSION_DEFAULT_LABEL;
+
+      vector<string> screens;
+
+      unsigned int currentScreenId;
+
+      void displayCurrentScreen();
+
+   };
+
+#endif /* VERSIONINFOMENUITEMACTION_H_ */
