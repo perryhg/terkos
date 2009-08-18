5a7ec760186a2d797077b5285322bce992e744f8
garthabrindoid <garthabrindoid@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-08-18 14:50:33 +0000

    Can now start and stop wireless via the LCD menu.

--- src/lcdmenu/src/WirelessStatusMenuItemAction.cpp
+++ src/lcdmenu/src/WirelessStatusMenuItemAction.cpp
@@ -17,13 +17,15 @@ const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_ACTION_CHO
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_CHOICE_OPTION1 = "wireless-disabled.choice.option1";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_CHOICE_OPTION2 = "wireless-disabled.choice.option2";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1 = "wireless-disabled.action.option1";
-const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION2 = "wireless-disabled.action.option2";
+const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_OPTION1_FAILURE = "wireless-disabled.option1.failure";
+const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1_BEFORE = "wireless-disabled.action.option1.before";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_PROMPT = "Wireless is off.";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_CANCEL = "Cancelled -- No settings changed";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_LABEL_OPTION1 = "Start   ";
 const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_LABEL_OPTION2 = "OK";
-const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1 = "You chose Start."; // TODO
-const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION2 = "Nothing to say here."; // TODO
+const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1 = "Wireless is now turned on.";
+const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_OPTION1_FAILURE = "Failed to start wireless.";
+const string WirelessStatusMenuItemAction::WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1_BEFORE = "Starting        wireless...";
 
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_ACTION_PROMPT = "wireless-enabled.action.prompt";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_CANCEL = "wireless-enabled.action.cancel";
@@ -31,12 +33,16 @@ const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_CHOICE_OPTI
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_CHOICE_OPTION2 = "wireless-enabled.choice.option2";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2 = "wireless-enabled.action.option2";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_OPTION1_FAILURE = "wireless-enabled.option1.failure";
+const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_OPTION2_FAILURE = "wireless-enabled.option2.failure";
+const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2_BEFORE = "wireless-enabled.action.option2.before";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_ACTION_PROMPT = "Wireless is on.";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_CANCEL = "Cancelled -- No settings changed";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_LABEL_OPTION1 = "Info  ";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_LABEL_OPTION2 = "Stop";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2 = "Wireless is now turned off.";
 const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_OPTION1_FAILURE = "Could not obtainwireless details";
+const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_OPTION2_FAILURE = "Failed to stop  wireless.";
+const string WirelessStatusMenuItemAction::WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2_BEFORE = "Stopping        wireless...";
 
 void WirelessStatusMenuItemAction::activate()
    {
@@ -83,14 +89,66 @@ bool WirelessStatusMenuItemAction::WirelessDisabledMenuItemAction::shouldOption1
 
 void WirelessStatusMenuItemAction::WirelessDisabledMenuItemAction::executeOption1Action()
    {
-   // TODO
-   cout << "WirelessDisabledMenuItemAction::executeOption1Action()" << endl;
+   isOnChoiceScreen = false;
+
+   getCharacterDisplay()->setText(getProperty(WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1_BEFORE, WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1_BEFORE));
+
+   // call the Perl script which enables wireless
+   try
+      {
+      // execute the script and return the results as a stream
+      redi::ipstream is("perl -I/opt/scripts /opt/scripts/enableWirelessNetworking.pl");
+
+      // parse the stream
+      Json::Reader reader;
+      Json::Value wirelessNetworkingStatusJson;
+      bool parsingSuccessful = reader.parse(is, wirelessNetworkingStatusJson, true);
+      is.close();
+
+      // make sure the enabling was successful
+      if (parsingSuccessful)
+         {
+         if (wirelessNetworkingStatusJson != Json::Value::null)
+            {
+            Json::Value isInstalled = wirelessNetworkingStatusJson["wireless-networking-status"]["is-installed"];
+            if (isInstalled != Json::Value::null)
+               {
+               if (isInstalled.asBool())
+                  {
+                  Json::Value wirelessInterface = wirelessNetworkingStatusJson["wireless-networking-status"]["wireless-interface"];
+                  if (wirelessInterface != Json::Value::null)
+                     {
+                     Json::Value isEnabled = wirelessInterface["is-enabled"];
+                     if (isEnabled != Json::Value::null && isEnabled.asBool())
+                        {
+                        getCharacterDisplay()->setText(getProperty(WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1, WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1));
+                        sleepThenPopUpToParentMenuItem(TwoOptionMenuItemAction::DEFAULT_MILLISECONDS_TO_SLEEP);
+
+                        return;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      else
+         {
+         cerr << "WirelessDisabledMenuItemAction::executeOption1Action(): failed to parse wireless networking status JSON:" << endl
+                  << reader.getFormatedErrorMessages();
+         }
+      }
+   catch (...)
+      {
+      cerr << "WirelessDisabledMenuItemAction::executeOption1Action(): failed to call script to enable wireless networking." << endl;
+      }
+
+   getCharacterDisplay()->setText(getProperty(WIRELESS_DISABLED_PROPERTY_OPTION1_FAILURE, WIRELESS_DISABLED_DEFAULT_OPTION1_FAILURE));
+   sleepThenPopUpToParentMenuItem(TwoOptionMenuItemAction::DEFAULT_MILLISECONDS_TO_SLEEP);
    }
 
 void WirelessStatusMenuItemAction::WirelessDisabledMenuItemAction::executeOption2Action()
    {
-   // TODO
-   cout << "WirelessDisabledMenuItemAction::executeOption2Action()" << endl;
+   isOnChoiceScreen = false;
    }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -118,7 +176,8 @@ void WirelessStatusMenuItemAction::WirelessEnabledMenuItemAction::executeOption1
 
 void WirelessStatusMenuItemAction::WirelessEnabledMenuItemAction::executeOption2Action()
    {
-   // TODO
+   getCharacterDisplay()->setText(getProperty(WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2_BEFORE, WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2_BEFORE));
+
    isOnChoiceScreen = false;
 
    // call the Perl script which disables wireless
@@ -130,13 +189,16 @@ void WirelessStatusMenuItemAction::WirelessEnabledMenuItemAction::executeOption2
       // In this case, we don't really care about the returned JSON status.  Just display
       // a message to the user and then pop up to the parent
       is.close();
+
+      getCharacterDisplay()->setText(getProperty(WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2, WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2));
       }
    catch (...)
       {
       cerr << "WirelessEnabledMenuItemAction::executeOption2Action(): failed to call script to disable wireless networking." << endl;
+      getCharacterDisplay()->setText(getProperty(WIRELESS_ENABLED_PROPERTY_OPTION2_FAILURE, WIRELESS_ENABLED_DEFAULT_OPTION2_FAILURE));
       }
 
-   getCharacterDisplay()->setText(getProperty(WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2, WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2));
+   sleepThenPopUpToParentMenuItem(TwoOptionMenuItemAction::DEFAULT_MILLISECONDS_TO_SLEEP);
    }
 // ---------------------------------------------------------------------------------------------------------------------
 
--- src/lcdmenu/src/WirelessStatusMenuItemAction.h
+++ src/lcdmenu/src/WirelessStatusMenuItemAction.h
@@ -36,14 +36,16 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
       static const string WIRELESS_DISABLED_PROPERTY_CHOICE_OPTION1;
       static const string WIRELESS_DISABLED_PROPERTY_CHOICE_OPTION2;
       static const string WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1;
-      static const string WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION2;
+      static const string WIRELESS_DISABLED_PROPERTY_OPTION1_FAILURE;
+      static const string WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1_BEFORE;
 
       static const string WIRELESS_DISABLED_DEFAULT_ACTION_PROMPT;
       static const string WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_CANCEL;
       static const string WIRELESS_DISABLED_DEFAULT_LABEL_OPTION1;
       static const string WIRELESS_DISABLED_DEFAULT_LABEL_OPTION2;
       static const string WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1;
-      static const string WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION2;
+      static const string WIRELESS_DISABLED_DEFAULT_OPTION1_FAILURE;
+      static const string WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1_BEFORE;
 
       static const string WIRELESS_ENABLED_PROPERTY_ACTION_PROMPT;
       static const string WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_CANCEL;
@@ -51,6 +53,8 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
       static const string WIRELESS_ENABLED_PROPERTY_CHOICE_OPTION2;
       static const string WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2;
       static const string WIRELESS_ENABLED_PROPERTY_OPTION1_FAILURE;
+      static const string WIRELESS_ENABLED_PROPERTY_OPTION2_FAILURE;
+      static const string WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2_BEFORE;
 
       static const string WIRELESS_ENABLED_DEFAULT_ACTION_PROMPT;
       static const string WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_CANCEL;
@@ -58,6 +62,8 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
       static const string WIRELESS_ENABLED_DEFAULT_LABEL_OPTION2;
       static const string WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2;
       static const string WIRELESS_ENABLED_DEFAULT_OPTION1_FAILURE;
+      static const string WIRELESS_ENABLED_DEFAULT_OPTION2_FAILURE;
+      static const string WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2_BEFORE;
 
       WirelessStatusMenuItemAction(void(*delObj)(void*), MenuItem* menuItem, MenuStatusManager* menuStatusManager,
                                    CharacterDisplay* characterDisplay, map<string, string>& properties) :
@@ -74,8 +80,10 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
                   = getProperty(WIRELESS_DISABLED_PROPERTY_CHOICE_OPTION2, WIRELESS_DISABLED_DEFAULT_LABEL_OPTION2);
          wirelessDisabledProperties[TwoOptionMenuItemAction::PROPERTY_ACTION_CHOSE_OPTION1]
                   = getProperty(WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1, WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1);
-         wirelessDisabledProperties[TwoOptionMenuItemAction::PROPERTY_ACTION_CHOSE_OPTION2]
-                  = getProperty(WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION2, WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION2);
+         wirelessDisabledProperties[WIRELESS_DISABLED_PROPERTY_OPTION1_FAILURE]
+                  = getProperty(WIRELESS_DISABLED_PROPERTY_OPTION1_FAILURE, WIRELESS_DISABLED_DEFAULT_OPTION1_FAILURE);
+         wirelessDisabledProperties[WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1_BEFORE]
+                  = getProperty(WIRELESS_DISABLED_PROPERTY_ACTION_CHOSE_OPTION1_BEFORE, WIRELESS_DISABLED_DEFAULT_ACTION_CHOSE_OPTION1_BEFORE);
 
          map<string, string> wirelessEnabledProperties;
          wirelessEnabledProperties[TwoOptionMenuItemAction::PROPERTY_ACTION_PROMPT]
@@ -90,6 +98,10 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
                   = getProperty(WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2, WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2);
          wirelessEnabledProperties[WIRELESS_ENABLED_PROPERTY_OPTION1_FAILURE]
                   = getProperty(WIRELESS_ENABLED_PROPERTY_OPTION1_FAILURE, WIRELESS_ENABLED_DEFAULT_OPTION1_FAILURE);
+         wirelessEnabledProperties[WIRELESS_ENABLED_PROPERTY_OPTION2_FAILURE]
+                  = getProperty(WIRELESS_ENABLED_PROPERTY_OPTION2_FAILURE, WIRELESS_ENABLED_DEFAULT_OPTION2_FAILURE);
+         wirelessEnabledProperties[WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2_BEFORE]
+                  = getProperty(WIRELESS_ENABLED_PROPERTY_ACTION_CHOSE_OPTION2_BEFORE, WIRELESS_ENABLED_DEFAULT_ACTION_CHOSE_OPTION2_BEFORE);
 
          wirelessFailureMenuItemAction
                   = new NoOpMenuItemAction(getProperty(STATUS_FAILURE_PROPERTY_ACTION_PROMPT, STATUS_FAILURE_DEFAULT_ACTION_PROMPT), NULL, menuItem, menuStatusManager, characterDisplay, properties);
@@ -215,11 +227,103 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
                // nothing to do
                }
 
+            void activate()
+               {
+               TwoOptionMenuItemAction::activate();
+               isOnChoiceScreen = true;
+               }
+
+            void start()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::start();
+                  }
+               else
+                  {
+                  activate();
+                  }
+               }
+
+            void stop()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::stop();
+                  }
+               else
+                  {
+                  activate();
+                  }
+               }
+
+            void upEvent()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::upEvent();
+                  }
+               }
+
+            void downEvent()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::downEvent();
+                  }
+               }
+
+            void rightEvent()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::rightEvent();
+                  }
+               }
+
+            void leftEvent()
+               {
+               if (isOnChoiceScreen)
+                  {
+                  TwoOptionMenuItemAction::leftEvent();
+                  }
+               }
+
          protected:
 
             bool shouldOption1BeSelectedUponActivation() const;
             void executeOption1Action();
             void executeOption2Action();
+
+            virtual bool shouldDisplayTextThenExitToParentWhenAction1IsComplete()
+               {
+               return false;
+               }
+
+            virtual bool shouldDisplayTextThenExitToParentWhenAction2IsComplete()
+               {
+               return true;
+               }
+
+            virtual int getMillisecondsToSleepBeforeExitingToParentAfterAction2IsComplete()
+               {
+               return 0;
+               }
+
+            virtual int getMillisecondsToSleepBeforeExitingToParentAfterCancel()
+               {
+               return 0;
+               }
+
+         private:
+
+            bool isOnChoiceScreen;
+
+            void sleepThenPopUpToParentMenuItem(const int millisecondsToSleep)
+               {
+               usleep(1000 * millisecondsToSleep);
+               CharacterDisplayMenuItemAction::stop();
+               }
 
          };
 
@@ -314,12 +418,7 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
 
             virtual bool shouldDisplayTextThenExitToParentWhenAction2IsComplete()
                {
-               return true;
-               }
-
-            virtual int getMillisecondsToSleepBeforeExitingToParentAfterAction1IsComplete()
-               {
-               return 0;
+               return false;
                }
 
             virtual int getMillisecondsToSleepBeforeExitingToParentAfterCancel()
@@ -330,7 +429,14 @@ class WirelessStatusMenuItemAction : public WirelessStatusCheckingMenuItemAction
          private:
 
             WirelessStatusMenuItemAction* parentMenuItemAction;
+
             bool isOnChoiceScreen;
+            
+            void sleepThenPopUpToParentMenuItem(const int millisecondsToSleep)
+               {
+               usleep(1000 * millisecondsToSleep);
+               CharacterDisplayMenuItemAction::stop();
+               }
 
          };
    };
