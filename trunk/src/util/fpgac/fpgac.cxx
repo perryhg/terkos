d39518b2a088c53c19596545c95d5da2c70f2ca2
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-09-08 14:58:15 +0000

    fixed lcd bug?

--- trunk/src/util/fpgac/fpgac.cxx
+++ trunk/src/util/fpgac/fpgac.cxx
@@ -3,81 +3,82 @@
 #include "memmap.h"
 
 int main(int argc, char **argv)
-	{
-	int i, res;
-	CMemMap gpio(0x80840000, 0xcc);
-	CMemMap ssp(0x808a0000, 0x18);
-	unsigned char bs[0x20000];
-	unsigned long len = 0x20000;
-	SBitstreamInfo info;
+{
+  int i, res;
+  CMemMap gpio(0x80840000, 0xcc);
+  CMemMap ssp(0x808a0000, 0x18);
+  unsigned char bs[0x20000];
+  unsigned long len = 0x20000;
+  SBitstreamInfo info;
 	
-	if (argc<2)
-		{
-		printf("Usage: config <file.bit>\n");
-		return -1;
-		}
+  if (argc<2)
+    {
+      printf("Usage: config <file.bit>\n");
+      return -1;
+    }
 		
-	std::ifstream is(argv[1], std::ios::binary);
+  std::ifstream is(argv[1], std::ios::binary);
 	
-	if (is.fail() || is.eof())
-		{
-		printf("Cannot open file\n");
-		return -1;
-		}
+  if (is.fail() || is.eof())
+    {
+      printf("Cannot open file\n");
+      return -1;
+    }
 	
-	res = CBitstream::ParseBitstream(is, bs, &len, &info);
-	if (res<0)
-		{
-		printf("Cannot parse bitstream\n");
-		return -1;
-		}
+  res = CBitstream::ParseBitstream(is, bs, &len, &info);
+  if (res<0)
+    {
+      printf("Cannot parse bitstream\n");
+      return -1;
+    }
 	
-	*gpio.Uint(0x10) = 0x0000000a; // set PA ddr
-	*gpio.Uint(0x00) = 0x00000008; // set PROG to high (run)
-	*gpio.Uint(0x14) = 0x00000003; // set PB ddr
-	*gpio.Uint(0x04) = 0x00000003; // set M0, M1 to high
+  *gpio.Uint(0x10) |= 0x00000002; // set PA ddr
+  *gpio.Uint(0x10) &= ~0x00000084; // set PA ddr
+  *gpio.Uint(0x00) &= ~0x00000002; // set PROG to high (run)
+  *gpio.Uint(0x14) |= 0x00000003; // set PB ddr
+  *gpio.Uint(0x04) |= 0x00000003; // set M0, M1 to high
 		
-	*ssp.Uint(0x00) = 0x00000007; // sspcr0
-	*ssp.Uint(0x04) = 0x00000010; // sspcr1
-	*ssp.Uint(0x10) = 0x00000002; // sspcpsr
-	*ssp.Uint(0x14) = 0x00000000; // sspiir
+  *ssp.Uint(0x00) = 0x00000007; // sspcr0
+  *ssp.Uint(0x04) = 0x00000010; // sspcr1
+  *ssp.Uint(0x10) = 0x00000002; // sspcpsr
+  *ssp.Uint(0x14) = 0x00000000; // sspiir
 
-	*gpio.Uint(0x00) = 0x0000000a; // set PROG to low (reset)
-	usleep(1);
-	*gpio.Uint(0x00) = 0x00000008; // set PROG to high (run)
-	// wait for INIT to go high
-	while((*gpio.Uint(0x00)&0x80)==0);
-	if (*gpio.Uint(0x00)&0x04)
-		{
-		printf("Done has not been cleared\n");
-		return -1;
-		}
+  *gpio.Uint(0x00) |= 0x00000002; // set PROG to low (reset)
+  usleep(1);
+  *gpio.Uint(0x00) &= ~0x00000002; // set PROG to high (run)
+  // wait for INIT to go high
+  while((*gpio.Uint(0x00)&0x80)==0);
+  if (*gpio.Uint(0x00)&0x04)
+    {
+      printf("Done has not been cleared\n");
+      return -1;
+    }
 			
-	for (i=0; i<(int)len; i++)
-		{
-		*ssp.Uint(0x08) = bs[i];	
-		while((*ssp.Uint(0x0c)&1)==0);
-		if ((*gpio.Uint(0x00)&0x80)==0)
-			{
-			printf("Configuration error\n");
-			return -1;
-			}
-		} 
-
-	// run extra cycles
-	for (i=0; i<10; i++)
-		{
-		*ssp.Uint(0x08) = 0xff;
-		while((*ssp.Uint(0x0c)&1)==0);
-		} 
-
-	usleep(100000);
-	if (*gpio.Uint(0x00)&0x04)
-		printf("Configuration successful\n");
-	else
-		{
-		printf("Configuration failed\n");
-		return -1;
-		}
-	return 0;
+  for (i=0; i<(int)len; i++)
+    {
+      *ssp.Uint(0x08) = bs[i];	
+      while((*ssp.Uint(0x0c)&1)==0);
+      if ((*gpio.Uint(0x00)&0x80)==0)
+	{
+	  printf("Configuration error\n");
+	  return -1;
 	}
+    } 
+
+  // run extra cycles
+  for (i=0; i<10; i++)
+    {
+      *ssp.Uint(0x08) = 0xff;
+      while((*ssp.Uint(0x0c)&1)==0);
+    } 
+
+  usleep(100000);
+  if (*gpio.Uint(0x00)&0x04)
+    printf("Configuration successful\n");
+  else
+    {
+      printf("Configuration failed\n");
+      return -1;
+    }
+  return 0;
+}
