e542e5483c83b7d5f19ce5c7a7e6619edf8ca25c
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2009-10-12 18:04:00 +0000

    more tests

--- src/factory/test/test.cxx
+++ src/factory/test/test.cxx
@@ -11,13 +11,19 @@
 #include "keypad.h"
 #include "qeleds.h"
 #include "qeaudio.h"
+#include "qeanalog.h"
+#include "qepower.h"
 
 CTextLcd *g_lcd;
 CKeypad *g_kp;
 CQELEDController *g_led;
 CQEMotorTraj *g_motor;
-int g_serial;
+C9302Hardware *g_9302;
+CQEAudioController *g_audio;
+CQEAnalog *g_analog;
+CQEPower *g_power;
 
+int g_serial;
 int serialInit()
 {
   struct termios newtio;
@@ -66,14 +72,39 @@ int testSerial()
     }
 }
 
+
 int testLCD()
 {
-  int key;
+  int i;
 
   g_lcd->Clear();
   g_lcd->printf("LCD testLCD test");
   g_lcd->MoveCursor(1, 0);
   g_lcd->printf("LCD testLCD test");
+
+  while(1)
+    {
+      g_lcd->SetProperty(TL_PROP_BACKLIGHT, true);
+      for (i=0; i<50; i++)
+	{
+	  usleep(100);
+	  if (g_kp->GetKey(false))
+	    {
+	      g_lcd->SetProperty(TL_PROP_BACKLIGHT, true);
+	      return 0;
+	    }
+	}
+      g_lcd->SetProperty(TL_PROP_BACKLIGHT, false);
+      for (i=0; i<50; i++)
+	{
+	  usleep(100);
+	  if (g_kp->GetKey(false))
+	    {
+	      g_lcd->SetProperty(TL_PROP_BACKLIGHT, true);
+	      return 0;
+	    }
+	}
+    }
 }
  
 int testKeypad()
@@ -302,8 +333,51 @@ int testMotors()
 	}
 
     }
- }
+}
 
+int testI2C()
+{
+  g_lcd->Clear();
+  g_lcd->printf("I2C test");
+
+  while(1)
+    {
+      if (g_kp->GetKey(false))
+	return 0;
+
+      g_lcd->MoveCursor(1, 0);
+
+      // set data in, clk low, loopback should produce 0, test data in, clk out
+      *g_9302->m_fpga.Ushort(0x480) = 0x0000;
+      usleep(100);
+      if (*g_9302->m_fpga.Ushort(0x480)!=0)
+	{
+	  g_lcd->printf("FAIL");
+	  continue;
+	}
+
+      // set data out and low, clk high, loopback should produce 4, test clk in
+      *g_9302->m_fpga.Ushort(0x480) = 0x0006;
+      usleep(100);
+      if (*g_9302->m_fpga.Ushort(0x480)!=4)
+	{
+	  g_lcd->printf("FAIL");
+	  continue;
+	}
+
+      // set data in, clk high, loopback should produce 3
+      *g_9302->m_fpga.Ushort(0x480) = 0x0002;
+      usleep(100);
+      if (*g_9302->m_fpga.Ushort(0x480)!=3)
+	{
+	  g_lcd->printf("FAIL");
+	  continue;
+	}
+
+      g_lcd->printf("PASS");
+    }
+}
+ 
 int numDevs()
 {
   int n = 0;
@@ -319,6 +393,320 @@ int numDevs()
   return n;
 }
 
+int testUSB()
+{
+  int n, nPrev = numDevs();
+
+  g_lcd->Clear();
+  g_lcd->printf("USB test");
+
+  while(1)
+    {
+      g_lcd->MoveCursor(1, 0);      
+      n = numDevs();
+      if (n-nPrev>0)
+	g_lcd->printf("Insert");
+      else if (n-nPrev<0)
+	g_lcd->printf("Pass  ");
+      nPrev = n;
+
+      if (g_kp->GetKey(false))
+	return 0;
+
+      usleep(100);
+    }
+}
+
+int testAudio()
+{
+  g_lcd->Clear();
+  g_lcd->printf("Audio test");
+
+  while(1)
+    {
+      g_audio->playTone(700, 64, 150);
+
+      if (g_kp->GetKey(false))
+	return 0;
+
+      g_audio->playTone(350, 64, 150);
+
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+#define VOLTAGE_TOL    500
+#define WITHIN_TOL(v, ref)  (((v)-(ref))<VOLTAGE_TOL && ((v)-(ref))>-VOLTAGE_TOL)
+
+int testPower()
+{
+  int battv, v, error;
+
+  g_lcd->Clear();
+  g_lcd->printf("Power test");
+
+  while(1)
+    {
+      error = 0;
+      g_lcd->MoveCursor(1, 0);      
+      battv = g_analog->GetADVoltage(24);
+
+      v = g_analog->GetADVoltage(16);
+      if (WITHIN_TOL(v, battv))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(17);
+      if (WITHIN_TOL(v, battv))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(18);
+      if (WITHIN_TOL(v, battv))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(19);
+      if (WITHIN_TOL(v, battv))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(20);
+      if (WITHIN_TOL(v, battv))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(22);
+      if (WITHIN_TOL(v, 5000))
+	g_lcd->printf("\xfc");
+      else 
+	{
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+      v = g_analog->GetADVoltage(23);
+      if (WITHIN_TOL(v, 5000))
+	g_lcd->printf("\xfc");
+      else
+	{ 
+	  g_lcd->printf("X");
+	  error = 1;
+	}
+
+      if (error)
+	g_lcd->printf(" FAIL");
+      else
+	g_lcd->printf(" PASS");
+
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+int testAnalog()
+{
+  int i, v, error;
+
+  g_lcd->Clear();
+  g_lcd->printf("Analog test");
+
+  while(1)
+    {
+      error = 0;
+      g_lcd->MoveCursor(1, 0);      
+
+      for (i=15; i>=0; i--)
+	{
+	  v = g_analog->GetADVoltage(i);
+	  if (WITHIN_TOL(v, 5000))
+	    g_lcd->printf("\xfc");
+	  else 
+	    {
+	      g_lcd->printf("X");
+	      error = 1;
+	    }
+	}
+      g_lcd->MoveCursor(0, 12); 
+      if (error)
+	g_lcd->printf("FAIL");
+      else
+	g_lcd->printf("PASS");
+
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+int testBackup()
+{
+  g_lcd->Clear();
+  g_lcd->printf("Backup batt test");
+  
+  while(1)
+    {
+      g_lcd->MoveCursor(1, 0);      
+      if (g_analog->GetADVoltage(21)>6800 &&
+	  g_analog->GetADVoltage(24)<5000)
+	g_lcd->printf("PASS");
+
+      usleep(1000);
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+#define D_DATA (*g_9302->m_fpga.Ushort(0x440))
+#define D_DIR  (*g_9302->m_fpga.Ushort(0x440+2))
+
+int testDigital()
+{
+  int i;
+  unsigned short error;
+
+  g_lcd->Clear();
+  g_lcd->printf("Digital test");
+
+  while(1)
+    {
+      error = 0;
+      D_DIR = 0x00ff;
+      
+      D_DATA = 0x00aa;   
+      usleep(1);
+      if ((D_DATA&0xff00)!=0xaa00)
+	error |= (D_DATA&0xff00)^0xaa00;
+
+      D_DATA = 0x0055;      
+      usleep(1);
+      if ((D_DATA&0xff00)!=0x5500)
+	error |= (D_DATA&0xff00)^0x5500;
+
+      D_DIR = 0xff00;
+      
+      D_DATA = 0xaa00;   
+      usleep(1);
+      if ((D_DATA&0x00ff)!=0x00aa)
+	error |= (D_DATA&0x00ff)^0x00aa;
+
+      D_DATA = 0x5500;      
+      usleep(1);
+      if ((D_DATA&0x00ff)!=0x0055)
+	error |= (D_DATA&0x00ff)^0x0055;
+
+      g_lcd->MoveCursor(0, 12);      
+      if (error)
+	g_lcd->printf("FAIL");
+      else
+	g_lcd->printf("PASS");
+
+      g_lcd->MoveCursor(1, 0);      
+      for (i=0; i<16; i++)
+	{
+	  if (error&0x8000)
+	    g_lcd->printf("X");
+	  else
+	    g_lcd->printf("\xfc");
+	  error <<= 1;
+	}
+	    
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+#undef D_DATA
+#undef D_DIR
+#define D_DATA (*g_9302->m_fpga.Ushort(0x400))
+#define D_DIR  (*g_9302->m_fpga.Ushort(0x400+2))
+
+int testServo()
+{
+  int i;
+  unsigned short error;
+
+  g_lcd->Clear();
+  g_lcd->printf("Servo test");
+
+  while(1)
+    {
+      error = 0;
+      D_DIR = 0x003f;
+      
+      D_DATA = 0x002a;   
+      usleep(1);
+      if ((D_DATA&0x0fc0)!=0x0a80)
+	error |= (D_DATA&0x0fc0)^0x0a80;
+
+      D_DATA = 0x0015;      
+      usleep(1);
+      if ((D_DATA&0x0fc0)!=0x0540)
+	error |= (D_DATA&0x0fc0)^0x0540;
+
+      D_DIR = 0x0fc0;
+      
+      D_DATA = 0x0a80;   
+      usleep(1);
+      if ((D_DATA&0x003f)!=0x002a)
+	error |= (D_DATA&0x003f)^0x002a;
+
+      D_DATA = 0x0540;      
+      usleep(1);
+      if ((D_DATA&0x003f)!=0x0015)
+	error |= (D_DATA&0x003f)^0x0015;
+
+      g_lcd->MoveCursor(0, 12);      
+      if (error)
+	g_lcd->printf("FAIL");
+      else
+	g_lcd->printf("PASS");
+
+      g_lcd->MoveCursor(1, 0);      
+      for (i=0; i<12; i++)
+	{
+	  if (error&0x800)
+	    g_lcd->printf("X");
+	  else
+	    g_lcd->printf("\xfc");
+	  error <<= 1;
+	}
+	    
+      if (g_kp->GetKey(false))
+	return 0;
+    }
+}
+
+int testPowerDown()
+{
+  g_lcd->Clear();
+  g_lcd->printf("Power down test");
+  g_lcd->MoveCursor(1, 0);      
+  g_lcd->printf("(Press OK)");
+
+  while(1)
+    {
+      if (g_kp->GetKey(false)==KP_KEY_OK)
+	g_power->PowerOff();
+      if (g_kp->GetKey(false))
+	return 0;
+     }
+}
+
 int main(int argc, char **argv)
 {
   int i = 0;
@@ -330,20 +718,6 @@ int main(int argc, char **argv)
   audio.playTone(700, 255, 500);
 #endif
 
-#if 0
-  int n, nPrev = numDevs();
-
-  while(1)
-    {
-      n = numDevs();
-      if (n-nPrev>0)
-	printf("insert\n");
-      else if (n-nPrev<0)
-	printf("remove\n");
-      nPrev = n;
-      usleep(100);
-    }
-#endif
 #if 0
   FILE *fp;
   while(1)
@@ -358,10 +732,14 @@ int main(int argc, char **argv)
   g_kp = CKeypad::GetPtr();
   g_lcd = CTextLcd::GetPtr();
   g_led = CQELEDController::GetPtr();
+  g_9302 = C9302Hardware::GetPtr();
+  g_audio = CQEAudioController::GetPtr();
+  g_analog = CQEAnalog::GetPtr();
+  g_power = CQEPower::GetPtr();
 
   serialInit();
-  // testSerial();
 
+  g_lcd->SetProperty(TL_PROP_BACKLIGHT, true);
   g_led->SetLED(LED_ROBOT, LED_COLOR_OFF);
   g_led->SetLED(LED_VEXNET, LED_COLOR_OFF);
   g_led->SetLED(LED_GAME, LED_COLOR_OFF);
@@ -398,12 +776,44 @@ int main(int argc, char **argv)
 	  break;
 
 	case 4:
+	  testAudio();
+	  break;
+
+	case 5:
+	  testDigital();
+	  break;
+	  
+	case 6:
+	  testServo();
+	  break;
+
+	case 7:
+	  testAnalog();
+	  break;
+
+	case 8:
+	  testI2C();
+	  break;
+
+	case 9:
 	  testSerial();
+	  break;
+
+	case 10:
+	  testUSB();
+	  break;
+
+	case 11:
+	  testBackup();
+	  break;
+
+	case 12: 
+	  testPowerDown();
 	  break;
 	}
       
       key = g_kp->GetKey();
-      if (key==KP_KEY_DOWN && i<4)
+      if (key==KP_KEY_DOWN && i<12)
 	i++;
       else if (key==KP_KEY_UP && i>0)
 	i--;
