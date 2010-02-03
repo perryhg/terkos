60ddfe730d5cfe2c4122f1128d0268be1655d871
rich.m.legrand <rich.m.legrand@82d350b0-1e1e-11de-b69a-45dc1c5e4070>
2010-02-03 20:10:38 +0000

    added interrupts to qegpioint class

--- trunk/src/libqwerk/qegpioint.cxx
+++ trunk/src/libqwerk/qegpioint.cxx
@@ -13,12 +13,22 @@
 // end license header
 //
 
+#include <unistd.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include "../module/qeinterrupt/qeinterrupt.h"
 #include "qegpioint.h"
+
 
 SINGLETON_REGISTER(CQEGpioInt);
 
+int CQEGpioInt::m_fd[QEG_NUM_IO];
+void (*CQEGpioInt::m_callback[QEG_NUM_IO])(unsigned int, struct timeval *);
+
 CQEGpioInt::CQEGpioInt()
 {
+  int i;
+
   m_p9302hw = C9302Hardware::GetPtr();
   if (m_p9302hw->GetBitstreamMajorVersion()!=0xa0)
     throw std::runtime_error("wrong FPGA bitstream version");
@@ -26,6 +36,15 @@ CQEGpioInt::CQEGpioInt()
   m_data = m_p9302hw->m_fpga.Ushort(QEG_DEFAULT_BASE); 
   m_dataDir = m_data + 1;
   m_intMode = m_data + 2;
+
+  for (i=0; i<QEG_NUM_IO; i++)
+    {
+      m_fd[i] = -1;
+      m_callback[i] = NULL;
+    }
+
+  // register callback
+  signal(SIGIO, SigHandler);  
 }
   
 CQEGpioInt::~CQEGpioInt()
@@ -123,3 +142,72 @@ int CQEGpioInt::SetProperty(int property, long value)
 
   return PROP_OK;
 }
+
+int CQEGpioInt::RegisterCallback(unsigned int io, void (*callback)(unsigned int, struct timeval *))
+{
+  int oflags;
+  char devbuf[32];
+  const char *dev = "/dev/qeint";
+
+  if (io>=QEG_NUM_IO)
+    return -1;
+
+  // open interrupt device
+  sprintf(devbuf, "%s%d", dev, io+16);
+  m_fd[io] = open(devbuf, O_RDWR); 
+  if (m_fd[io]<0)
+    return -1;
+
+  if (fcntl(m_fd[io], F_SETOWN, getpid())<0)
+    return -1;
+  oflags = fcntl(m_fd[io], F_GETFL);
+  fcntl(m_fd[io], F_SETFL, oflags | FASYNC);
+  
+  // clear status
+  ioctl(m_fd[io], QEINT_IOC_RESET_STATUS); 
+
+  // assuming the following assignment is atomic...
+  m_callback[io] = callback;
+
+  return 0;
+}
+
+int CQEGpioInt::UnregisterCallback(unsigned int io)
+{
+  if (io>=QEG_NUM_IO)
+    return -1;
+
+  // assuming the following assignment is atomic...
+  m_callback[io] = NULL;
+  // close device
+  close(m_fd[io]);
+  m_fd[io] = -1;
+
+  return 0;
+}
+
+void CQEGpioInt::SigHandler(int signum)
+{
+  int i;
+  char status;
+  struct timeval tv;
+
+  for (i=0; i<QEG_NUM_IO; i++)
+    {
+      if (m_callback[i]!=NULL && m_fd[i]!=-1)
+	{
+	  // check to see if interrupt has occurred on this I/O 
+	  ioctl(m_fd[i], QEINT_IOC_READ_STATUS, &status);
+	  if (status)
+	    {
+	      // clear status
+	      ioctl(m_fd[i],  QEINT_IOC_RESET_STATUS);
+	      // read time
+	      read(m_fd[i], (void *)&tv, sizeof(tv));
+	      // callback 
+	      (*m_callback[i])(i, &tv);
+	    }
+	}
+    }
+}
+
